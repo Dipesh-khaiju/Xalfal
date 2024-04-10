435a028592afd4cd1ef4e9875f1a7a53213c210f
Dipesh Khaiju <khaijudipesh@gmail.com>
2024-04-10 20:01:12 +0545

    cors isssue

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
-import {app,server,io} from "./socket/socket.js"
-import express from "express"
+import { app, server, io } from "./socket/socket.js";
+import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
+import cors from "cors"; // Import cors
 
 import authRoutes from "./routes/authRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
@@ -11,25 +11,24 @@ import userRoutes from "./routes/userRoutes.js";
 // https://xalfal-app.onrender.com hosted backend url
 import mongoConnect from "./db/conn.js";
 
-
 const port = process.env.PORT || 3000;
 
 dotenv.config();
 
 //middlewares
-app.use(express.json()); // to parse icoming req from req.body
+app.use(express.json()); // to parse incoming req from req.body
 app.use(cookieParser()); // to access the cookies
+app.use(cors()); // Use cors middleware to allow requests from all origins
 
-app.use("/api/auth",authRoutes);
-app.use("/api/messages",messageRoutes);
-app.use("/api/users",userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
-app.get("/",(req,res)=>{
-    res.send("home page");
+app.get("/", (req, res) => {
+  res.send("home page");
 });
 
-
-server.listen(port,()=>{
-    mongoConnect();
-    console.log(`Listening on port ${port}`)
-})
+server.listen(port, () => {
+  mongoConnect();
+  console.log(`Listening on port ${port}`);
+});
