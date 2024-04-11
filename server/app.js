72bb1a778069e10f15752b8c42c8e2da8d88be09
Dipesh Khaiju <khaijudipesh@gmail.com>
2024-04-11 09:41:30 +0545

    production ready done

--- server/app.js
+++ server/app.js
@@ -3,15 +3,17 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors"; // Import cors
+import path from "path";
 
 import authRoutes from "./routes/authRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 
-// https://xalfal-app.onrender.com hosted backend url
+
 import mongoConnect from "./db/conn.js";
 
 const port = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -19,13 +21,29 @@ dotenv.config();
 app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true  // Include credentials (cookies, authorization headers, etc.) in the CORS request
-  })); // Use cors middleware to allow requests from all origins
+  }));
+
 app.use(express.json()); // to parse incoming req from req.body
 app.use(cookieParser()); // to access the cookies
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+
+// Set the correct MIME type for JavaScript files
+app.use((req, res, next) => {
+  if (req.url.endsWith(".js")) {
+      res.type("application/javascript");
+  }
+  next();
+});
+
+app.use(express.static(path.join(__dirname,"../client/dist")))
+
+app.get("*",(req,res)=>{
+  res.sendFile(path.join(__dirname,"../","client","dist","index.html"))
+})
 
 app.get("/", (req, res) => {
   res.send("home page");
